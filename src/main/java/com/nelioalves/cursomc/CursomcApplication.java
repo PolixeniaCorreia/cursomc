eb5606488ee762b4fba9dd9a151a2cdf6b4e3059
PolixeniaCorreia <polixeniacorreia@gmail.com>
2018-08-27 15:44:32 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -78,7 +78,7 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	cli1.getTelefone().addAll(Arrays.asList("27363323","9383989"));
 	
-	Endereco e1 = new Endereco (null,"RUa FLores","300","Apto 303","Jardim","38220834",cli1,c1);
+	Endereco e1 = new Endereco (null,"Rua FLores","300","Apto 303","Jardim","38220834",cli1,c1);
 	Endereco e2 = new Endereco (null,"Avenida Matos","105","Sala 800","Centro","38777012",cli1,c2);
 	
 	cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
